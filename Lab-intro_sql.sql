7c2e1abb1ba26afc312f69d700479586bb70089b
N4d1n3 <75038754+N4d1n3@users.noreply.github.com>
2021-01-20 16:03:10 +0100

    Add files via upload

--- Lab-intro_sql.sql
+++ Lab-intro_sql.sql
@@ -1,13 +1,45 @@
 use sakila;
-SELECT title
-FROM  film;
-SELECT DISTINCT name as language
-FROM language;
-SELECT return_date
+
+-- 1
+SELECT first_name, last_name
+FROM actor
+WHERE first_name = 'Scarlett';
+-- 2
+SELECT COUNT(*) from sakila.film;
+
+SELECT rental_duration, film_id
+FROM film;
+-- 3
+SELECT max(rental_duratio) as max_duration, min(rental_duration) as min_duration 
+FROM sakila.film; 
+
+-- 4 
+SELECT floor(avg(length)/60) as hours, round(avg(length)%60) as minutes
+FROM sakila.film;
+
+-- 5 
+SELECT COUNT(distinct last_name)
+FROM actor; 
+
+-- 6
+SELECT DATEDIFF (MAX(return_date), MIN(rental_date)) as datenew
+FROM rental; 
+
+
+-- 7
+SELECT *, date_format(rental_date, '%M') as month, date_format(rental_date, '%W') as weekday
+FROM rental
+LIMIT 20;
+
+-- 8
+SELECT *, case when date_format(rental_date, '%W') in ('Saturday', 'Sunday')
+	then 'weekend'
+    else 'workday' end as day_type
 FROM rental;
-SELECT first_name
-FROM staff;
-SELECT city
-FROM city;
-Select COUNT(rental_date)
-FROM rental;
+
+-- 9
+
+SELECT COUNT(*)
+FROM 
+
+
